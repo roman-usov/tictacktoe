6e574facfe3e0d3661276da9e605c7669ea83d07
roman-usov <66104246+roman-usov@users.noreply.github.com>
2023-07-02 16:55:13 +0400

    initial commit

---
+++ __tests__/application.test.js
@@ -0,0 +1,49 @@
+import '@testing-library/jest-dom';
+import fs from 'fs';
+import path from 'path';
+import testingLibraryDom from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
+
+import run from '../src/application.js';
+
+const { screen } = testingLibraryDom;
+
+const getCell = (rowIndex, cellIndex) => {
+  const table = document.querySelector('table');
+  return table.rows.item(rowIndex).cells.item(cellIndex);
+};
+
+beforeAll(() => {
+  const initHtml = fs.readFileSync(path.join('__fixtures__', 'index.html')).toString();
+  document.body.innerHTML = initHtml;
+  run();
+});
+
+test('application', () => {
+  const root = screen.getByTestId('root');
+  const table = screen.getByRole('table');
+  expect(root).toContainElement(table);
+  expect(getCell(0, 0)).toHaveTextContent('s');
+  expect(getCell(1, 1)).toHaveTextContent('s');
+  expect(getCell(2, 2)).toHaveTextContent('s');
+});
+
+const cells = [
+  [2, 2, 'x'],
+  [1, 1, 'o'],
+  [1, 2, 'x'],
+  [2, 1, 'o'],
+  [2, 1, 'o'],
+  [0, 0, 'o'],
+  [1, 0, 'x'],
+  [2, 0, 'o'],
+  [0, 2, 'x'],
+];
+
+test.each(cells)('Row %s, column %s, set "%s"', async (row, column, symbol) => {
+  const cellBeforeClick = getCell(row, column);
+  await userEvent.click(cellBeforeClick);
+  const cellAfterClick = getCell(row, column);
+  const regex = new RegExp(`^${symbol}$`);
+  expect(cellAfterClick).toHaveTextContent(regex);
+});
---
+++ src/application.js
@@ -0,0 +1,18 @@
+const generateField = () => {
+  const tableEl = document.createElement('table');
+
+  tableEl.className = 'table-bordered';
+  for (let i = 0; i < 3; i += 1) {
+    const row = tableEl.insertRow();
+    for (let j = 0; j < 3; j += 1) {
+      const cell = row.insertCell();
+      cell.className = 'py-2 px-3';
+      cell.innerHTML = '<span class="invisible">s</span>';
+    }
+  }
+  return tableEl;
+};
+
+// BEGIN (write your solution here)
+
+// END
